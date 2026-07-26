a85df8609a17112c74fa8e820036c8d4fad5f636
sawan <sawan@example.com>
2026-07-26 16:04:10 +0530

    chore(nav): update Navbar navigation data structure

---
+++ src/components/layout/Navbar/NavigationData.ts
@@ -0,0 +1,32 @@
+export const navigation = [
+  {
+    title: "Destinations",
+    children: [
+      "Dubai",
+      "Maldives",
+      "Bali",
+      "Thailand",
+      "Singapore",
+      "Japan",
+    ],
+  },
+  {
+    title: "Packages",
+    children: [
+      "International",
+      "Domestic",
+      "Honeymoon",
+      "Family",
+      "Luxury",
+    ],
+  },
+  {
+    title: "Services",
+    children: [
+      "Flights",
+      "Hotels",
+      "Visa",
+      "Travel Insurance",
+    ],
+  },
+];
