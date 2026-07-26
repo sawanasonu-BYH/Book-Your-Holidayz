16e1b4424e6dc6061c976a14527897eaea9e528c
sawan <sawan@example.com>
2026-07-26 13:45:39 +0530

    feat: add PopularPackages to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/home/Hero";
 import FeaturedDestinations from "@/components/home/FeaturedDestinations";
+import PopularPackages from "@/components/home/PopularPackages";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <FeaturedDestinations />
+      <PopularPackages />
     </>
   );
 }
