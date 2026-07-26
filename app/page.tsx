abf229d05cd21c09e2c0f38433e6507361b2657c
sawan <sawan@example.com>
2026-07-26 12:47:58 +0530

    feat: render FeaturedDestinations on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/home/Hero";
+import FeaturedDestinations from "@/components/home/FeaturedDestinations";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <FeaturedDestinations />
     </>
   );
 }
